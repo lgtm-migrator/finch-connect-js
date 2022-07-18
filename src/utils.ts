85ac848aea0c48afdfc2efa64c58c5b87d2d7104
Yujia Cao <y77cao@Yujias-MBP.hitronhub.home>
2022-07-18 14:30:08 -0400

    js

--- src/utils.ts
+++ src/utils.ts
@@ -15,7 +15,7 @@ export const constructAuthUrl = (
     authUrl.searchParams.append('mode', 'employer');
     if (manual) authUrl.searchParams.append('manual', String(manual));
     if (sandbox) authUrl.searchParams.append('sandbox', String(sandbox));
-    if (SDK_VERSION) authUrl.searchParams.append('sdk_version', SDK_VERSION);
+    if (SDK_VERSION) authUrl.searchParams.append('sdk_version', `js-${SDK_VERSION}`);
 
     return authUrl.href;
 };
